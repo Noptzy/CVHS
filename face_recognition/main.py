e6071a0fda8bec363af4b96eff6a0f1940103e90
Noptzy <nopsy.zet@gmail.com>
2025-01-01 14:57:59 +0700

    update 1

--- face_recognition/main.py
+++ face_recognition/main.py
@@ -64,7 +64,7 @@ def main():
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    cap.set(cv2.CAP_PROP_FPS, 30)
+    cap.set(cv2.CAP_PROP_FPS, 60)
 
     if not cap.isOpened():
         print("Could not open webcam!")
